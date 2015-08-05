9bedf0c46dbaf0b72c6ab29fde7c487b9c760785
Brian <fredyfontaine@yahoo.co.uk>
2015-08-05 14:53:31 +0100

    tidy up and add main.js

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+﻿function fnExpandReferencesDiv() {
+    console.log("Button to check media query Clicked");
+    var mq = window.matchMedia("(min-width: 768px)");
+
+    if (mq.matches) { // window width is at least 768px
+        console.log(" window width is at least 500px");
+        document.getElementById("collapseReferences").className += " in";
+
+    }
+    else { // window width is less than 768px
+        console.log(" window width is less than 768px");
+    }
+}
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
